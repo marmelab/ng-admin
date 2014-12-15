88f22f44580477e4b5bd0acdcd4e35840635ef42
Emmanuel Quentin <manu.quentin@gmail.com>
2014-12-15 17:06:56 +0100

    Fix home link

--- src/javascripts/ng-admin/Main/component/controller/AppController.js
+++ src/javascripts/ng-admin/Main/component/controller/AppController.js
@@ -6,21 +6,27 @@ define(function () {
     /**
      *
      * @param {$scope}               $scope
+     * @param {$location}            $location
      * @param {NgAdminConfiguration} Configuration
      * @constructor
      */
-    var AppController = function ($scope, Configuration) {
+    var AppController = function ($scope, $location, Configuration) {
         this.$scope = $scope;
+        this.$location = $location;
         this.applicationName = Configuration().title();
 
         $scope.$on('$destroy', this.destroy.bind(this));
+    };
+
+    AppController.prototype.displayHome = function () {
+        this.$location.path('dashboard');
     };
 
     AppController.prototype.destroy = function () {
         this.$scope = undefined;
     };
 
-    AppController.$inject = ['$scope', 'NgAdminConfiguration'];
+    AppController.$inject = ['$scope', '$location', 'NgAdminConfiguration'];
 
     return AppController;
 });
