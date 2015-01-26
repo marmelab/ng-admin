412d8a37acb06ff3a2887ab618155b71496c1e86
Francois Zaninotto <fzaninotto@gmail.com>
2015-01-26 14:21:43 +0100

    Add functinoal test for edit view

--- src/javascripts/test/e2e/referenceSpec.js
+++ src/javascripts/test/e2e/referenceSpec.js
@@ -2,16 +2,34 @@
 describe('Reference fields', function () {
     'use strict';
 
-    beforeEach(function() {
-        browser.get(browser.baseUrl + '#/show/posts/1');
-    });
+    describe('in ShowView', function () {
+        beforeEach(function() {
+            browser.get(browser.baseUrl + '#/show/posts/1');
+        });
 
-    it('should display ReferencedList as a datagrid', function () {
-        $$('.ng-admin-field-comments th').then(function (inputs) {
-            expect(inputs.length).toBe(2);
+        it('should display ReferencedList as a datagrid', function () {
+            $$('.ng-admin-field-comments th').then(function (inputs) {
+                expect(inputs.length).toBe(2);
 
-            expect(inputs[0].getAttribute('class')).toBe('ng-scope ng-admin-column-id');
-            expect(inputs[1].getAttribute('class')).toBe('ng-scope ng-admin-column-body');
+                expect(inputs[0].getAttribute('class')).toBe('ng-scope ng-admin-column-id');
+                expect(inputs[1].getAttribute('class')).toBe('ng-scope ng-admin-column-body');
+            });
         });
     });
+
+    describe('in ListView', function () {
+        beforeEach(function() {
+            browser.get(browser.baseUrl + '#/show/posts/1');
+        });
+
+        it('should display ReferencedList as a datagrid', function () {
+            $$('.ng-admin-field-comments th').then(function (inputs) {
+                expect(inputs.length).toBe(2);
+
+                expect(inputs[0].getAttribute('class')).toBe('ng-scope ng-admin-column-id');
+                expect(inputs[1].getAttribute('class')).toBe('ng-scope ng-admin-column-body');
+            });
+        });
+    });
+
 });
