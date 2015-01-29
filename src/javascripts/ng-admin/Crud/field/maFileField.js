b46fce690fbc8d01f8953abc8fe21201dfac97f5
Jérôme Macias <jerome.macias@gmail.com>
2015-01-29 15:37:03 +0100

    Diplay all uploaded files

--- src/javascripts/ng-admin/Crud/field/maFileField.js
+++ src/javascripts/ng-admin/Crud/field/maFileField.js
@@ -8,7 +8,7 @@ define(function (require) {
      *
      * @example <ma-file-field field="field"></ma-file-field>
      */
-    function maFileField($upload, $compile, $timeout) {
+    function maFileField($upload, $timeout) {
         return {
             scope: {
                 'field': '&',
@@ -24,7 +24,15 @@ define(function (require) {
 
                     scope.multiple = uploadInformation.hasOwnProperty('multiple') ? uploadInformation.multiple : false;
                     scope.accept = uploadInformation.hasOwnProperty('accept') ? uploadInformation.accept : '*';
-                    scope.progress = 0;
+
+                    var files = scope.value ? scope.value.split(',') : [];
+                    scope.files = {};
+                    for (var file in files) {
+                        scope.files[files[file]] = {
+                            "name": files[file],
+                            "progress": 0
+                        };
+                    }
                 },
                 post: function(scope, element) {
                     var field = scope.field();
@@ -36,28 +44,37 @@ define(function (require) {
                         input[name] = attributes[name];
                     }
 
-                    scope.fileSelected = function(files) {
-                        var uploadParams, file;
+                    scope.fileSelected = function(selectedFiles) {
+                        if (!selectedFiles) {
+                            return;
+                        }
 
-                        for (var file in files) {
+                        var uploadParams;
+
+                        scope.files = {};
+                        for (var file in selectedFiles) {
                             uploadParams = angular.copy(field.config.uploadInformation);
-                            uploadParams.file = files[file];
+                            uploadParams.file = selectedFiles[file];
 
                             $upload
                                 .upload(uploadParams)
                                 .progress(function(evt) {
-                                    scope.value = files[file].name;
-                                    scope.progress = parseInt(100.0 * evt.loaded / evt.total);
+                                    scope.files[evt.config.file.name] = {
+                                        "name": evt.config.file.name,
+                                        "progress": Math.min(100, parseInt(100.0 * evt.loaded / evt.total))
+                                    };
                                 })
                                 .success(function(data, status, headers, config) {
-                                    scope.progress = 0;
-                                    scope.value = files[file].name;
+                                    scope.files[config.file.name] = {
+                                        "name": config.file.name,
+                                        "progress": 100
+                                    };
+                                    scope.value = Object.keys(scope.files).join(',');
                                 })
-                                .error(function(data) {
-                                    //error
-                                    console.log(data);
-                                    scope.progress = 0;
-                                    scope.value = null;
+                                .error(function(data, status, headers, config) {
+                                    delete scope.files[config.file.name];
+
+                                    scope.value = Object.keys(scope.files).join(',');
                                 });
                         }
                     };
@@ -70,27 +87,31 @@ define(function (require) {
                 }
             },
             template:
-'<div style="row">' +
+'<div class="row">' +
     '<div class="col-md-2">' +
-        '<a class="btn btn-default" ng-click="selectFile()" style="margin-left: -15px;">' +
+        '<a class="btn btn-default" ng-click="selectFile()">' +
             '<span>Browse</span>' +
         '</a>' +
     '</div>' +
-    '<div class="col-md-2" ng-show="progress" style="padding-top: 6px;">' +
-        '<div class="progress" style="margin-bottom: 0;">' +
-            '<div class="progress-bar" role="progressbar" aria-valuenow="{{ progress }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ progress }}%;">' +
-                '<span class="sr-only">{{ progress }}% Complete</span>' +
+    '<div class="col-md-10">' +
+        '<div class="row" ng-repeat="file in files track by $index">' +
+            '<div class="col-md-3" style="padding-top: 6px;">' +
+                '<div class="progress" style="margin-bottom: 0;" ng-if="file.progress">' +
+                    '<div class="progress-bar" role="progressbar" aria-valuenow="{{ file.progress }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ file.progress }}%;">' +
+                        '<span class="sr-only">{{ file.progress }}% Complete</span>' +
+                    '</div>' +
+                '</div>' +
             '</div>' +
+            '<div class="col-md-9" style="padding-top: 6px;"><small><em>{{ file.name }}<em><small></div>' +
         '</div>' +
     '</div>' +
-    '<div class="col-md-2" ng-show="value" style="padding-top: 6px;">{{ value }}</div>' +
-    '<input type="file" ng-multiple="multiple" accept="accept" ng-file-select ng-model="files" ng-file-change="fileSelected($files)"' +
-            'id="{{ name }}" name="{{ name }}" ng-required="v.required" style="display:none" />' +
-'</div>'
+'</div>' +
+'<input type="file" ng-multiple="multiple" accept="accept" ng-file-select ng-model="selectedFiles" ng-file-change="fileSelected($files)"' +
+        'id="{{ name }}" name="{{ name }}" ng-required="v.required" style="display:none" />'
         };
     }
 
-    maFileField.$inject = ['$upload', '$compile', '$timeout'];
+    maFileField.$inject = ['$upload', '$timeout'];
 
     return maFileField;
 });
