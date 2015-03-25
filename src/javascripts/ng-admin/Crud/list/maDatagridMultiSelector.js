06d8b477aebcffc294638651f996a451d25d1bba
ThieryMichel <thiery@marmelab.com>
2015-03-25 14:20:48 +0100

    consider selection to be complete even if order do not match

--- src/javascripts/ng-admin/Crud/list/maDatagridMultiSelector.js
+++ src/javascripts/ng-admin/Crud/list/maDatagridMultiSelector.js
@@ -3,6 +3,17 @@
 define(function () {
     'use strict';
 
+    var compareArray = function (a1, a2) {
+        if (a2.length !== a1.length) {
+            return false;
+        }
+
+        return a1.reduce(function (prev, cur) {
+            if(prev === false) { return prev; }
+            return a2.indexOf(cur) !== -1;
+        }, true);
+    };
+
     function DatagridMultiSelectorDirective() {
         return {
             restrict: 'E',
@@ -14,24 +25,31 @@ define(function () {
             template: '<input type="checkbox" ng-click="toggleSelectAll()" ng-model="selected" />',
             link: function (scope, element) {
 
-                scope.$watch('selection', function (selection) {
-                    var entries = JSON.stringify(scope.entries);
-                    selection = JSON.stringify(selection);
-                    if (selection === entries){
+                function updateState(selection, entries) {
+                    var equal = compareArray(entries, selection);
+
+                    if (equal){
                         scope.selected = true;
                         element.find('input').prop('indeterminate', false);
                         return;
                     }
-                    if (selection === '[]'){
+                    if (selection.length === 0){
                         scope.selected = false;
                         element.find('input').prop('indeterminate', false);
                         return;
                     }
-                    if (selection !== '[]' && selection !== entries){
+                    if (selection !== '[]' && !equal){
                         scope.selected = false;
                         element.find('input').prop('indeterminate', true);
                         return;
                     }
+                }
+
+                scope.$watch('selection', function (selection) {
+                    updateState(selection, scope.entries);
+                });
+                scope.$watch('entries', function (entries) {
+                    updateState(scope.selection, entries);
                 });
             }
         };
--- src/javascripts/test/unit/Crud/list/maDatagridMultiSelectorSpec.js
+++ src/javascripts/test/unit/Crud/list/maDatagridMultiSelectorSpec.js
@@ -41,6 +41,15 @@ define(function (require) {
             expect(element.children()[0].checked).toBe(true);
         });
 
+        it('checkbox should be true if entries correspond to selection even if order differ', function () {
+            scope.selection = [scope.entries[2], scope.entries[1], scope.entries[0]];
+            var element = $compile(directiveUsage)(scope);
+            scope.$digest();
+
+            expect(element.children()[0].indeterminate).toBe(false);
+            expect(element.children()[0].checked).toBe(true);
+        });
+
         it('checkbox should be false if selection is empty', function () {
             scope.selection = [];
             var element = $compile(directiveUsage)(scope);
@@ -50,7 +59,7 @@ define(function (require) {
             expect(element.children()[0].checked).toBe(false);
         });
 
-        it('checkbox should become indeterminate once selection stop corresponding to selection', function () {
+        it('checkbox should become indeterminate once selection stop corresponding to entries', function () {
             scope.selection = scope.entries;
             var element = $compile(directiveUsage)(scope);
             scope.$digest();
@@ -58,7 +67,23 @@ define(function (require) {
             expect(element.children()[0].indeterminate).toBe(false);
             expect(element.children()[0].checked).toBe(true);
 
-            scope.selection = scope.selection.pop();
+            scope.selection = [scope.selection.pop()];
+            scope.$digest();
+
+            expect(element.children()[0].indeterminate).toBe(true);
+            expect(element.children()[0].checked).toBe(false);
+        });
+
+        it('checkbox should become indeterminate once entries stop corresponding to selection', function () {
+            scope.selection = scope.entries;
+            var element = $compile(directiveUsage)(scope);
+            scope.$digest();
+
+            expect(element.children()[0].indeterminate).toBe(false);
+            expect(element.children()[0].checked).toBe(true);
+
+            scope.entries = scope.entries.concat(new Entry('entity', {some: 'value'}, 'entity_4'));
+
             scope.$digest();
 
             expect(element.children()[0].indeterminate).toBe(true);
