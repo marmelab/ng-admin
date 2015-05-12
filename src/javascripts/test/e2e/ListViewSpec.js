298a48160c1da8339efaafc59285f60df6c33280
ThieryMichel <thiery@marmelab.com>
2015-05-12 11:20:01 +0200

    add test

--- src/javascripts/test/e2e/ListViewSpec.js
+++ src/javascripts/test/e2e/ListViewSpec.js
@@ -29,4 +29,48 @@ xdescribe('ListView', function () {
             });
         });
     });
+
+    describe('ma-list-button', function () {
+        var listUrl;
+
+        beforeEach(function() {
+            listUrl = encodeURI(browser.baseUrl + '/#/comments/list?search={"post_id":"9"}&page=1');
+            browser.get(listUrl);
+        });
+
+        it('should restore the list with filter when used from edit', function () {
+            browser.executeScript('window.scrollTo(810, 481)').then(function () {
+                $$('ma-edit-button a').then(function (elements) {
+                    expect(elements[0].getText()).toBe(' Edit');
+                    elements[0].click();
+                    expect(browser.getCurrentUrl()).toBe(browser.baseUrl + '/#/comments/edit/2');
+                    $$('ma-list-button a').then(function (elements) {
+                        elements[0].click();
+                        expect(browser.getCurrentUrl()).toBe(listUrl);
+                    });
+                });
+            });
+        });
+
+        it('should restore the list with filter when used from delete', function () {
+            browser.get(listUrl);
+            browser.executeScript('window.scrollTo(810, 481)').then(function () {
+
+                $$('ma-delete-button a').then(function (elements) {
+                    expect(elements[0].getText()).toBe(' Delete');
+                    elements[0].click();
+                    expect(browser.getCurrentUrl()).toBe(browser.baseUrl + '/#/comments/delete/2');
+                    $$('button.btn-default').then(function (elements) {
+                        elements[0].click();
+                        expect(browser.getCurrentUrl()).toBe(browser.baseUrl + '/#/comments/edit/2');
+
+                        $$('ma-list-button a').then(function (elements) {
+                            elements[0].click();
+                            expect(browser.getCurrentUrl()).toBe(listUrl);
+                        });
+                    });
+                });
+            });
+        });
+    });
 });
