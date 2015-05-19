41baed3b233c1b6af92b997469219259c4f1eda7
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2015-05-19 11:33:42 +0200

    Cleaning up

--- src/javascripts/test/karma.conf.js
+++ src/javascripts/test/karma.conf.js
@@ -20,47 +20,9 @@ module.exports = function (config) {
             'test/unit/**/*.js'
         ],
         plugins: ['karma-webpack', 'karma-jasmine', 'karma-chrome-launcher', 'karma-phantomjs-launcher', 'karma-babel-preprocessor'],
-
-        //files: [
-        //    {pattern: 'bower_components/angular-mocks/angular-mocks.js', included: true},
-        //    {pattern: '../../node_modules/angular/angular.js', included: true},
-        //    {pattern: '../../node_modules/angular-numeraljs/dist/angular-numeraljs.js', included: true},
-        //    {pattern: '../../node_modules/codemirror/lib/codemirror.js', included: true},
-        //    {pattern: '../../node_modules/codemirror/addon/edit/closebrackets.js', included: true},
-        //    {pattern: '../../node_modules/codemirror/addon/edit/matchbrackets.js', included: true},
-        //    {pattern: '../../node_modules/codemirror/addon/lint/json-lint.js', included: true},
-        //    {pattern: '../../node_modules/codemirror/addon/lint/lint.js', included: true},
-        //    {pattern: '../../node_modules/codemirror/addon/selection/active-line.js', included: true},
-        //    {pattern: '../../node_modules/codemirror/mode/javascript/javascript.js', included: true},
-        //    {pattern: '../../node_modules/jsonlint/lib/jsonlint.js', included: true},
-        //    {pattern: '../../node_modules/numeral/numeral.js', included: true},
-        //
-        //    // ng-admin application files
-        //    {pattern: 'vendors.js', included: true},
-        //    {pattern: 'ng-admin/**/**/**/*.js', included: true},
-        //    {pattern: 'ng-admin/**/*/*.html', included: true},
-        //    {pattern: 'ng-admin/**/view/**/*.html', included: true},
-        //    {pattern: 'ng-admin/es6/lib/**/*.js', included: true},
-        //    {pattern: 'ng-admin/lib/**/*.js', included: true},
-        //    {pattern: 'ng-admin/lib/polyfill/bind.js', included: true},
-        //
-        //    // Test files
-        //    {pattern: 'test/function.bind.shim.js', included: true},
-        //    {pattern: 'test/mock/*.js', included: true},
-        //    {pattern: 'test/unit/**/**/*.js', included: true},
-        //
-        //    // Test bootstrap
-        //    'test/app-test.js'
-        //],
         preprocessors: {
-            //'ng-admin/es6/lib/**/*.js': 'babel',
             'test/**/*.js': 'webpack'
         },
-        //babelPreprocessor: {
-        //    options: {
-        //        modules: "amd"
-        //    }
-        //},
         webpackMiddleware: {
             noInfo: true
         },
