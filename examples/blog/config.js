cd5b9af1f7b612808459649290c4ad709e6c6fd2
Jérôme Macias <jerome.macias@gmail.com>
2015-06-10 11:04:38 +0200

    Fix typo in example custom page configuration

--- examples/blog/config.js
+++ examples/blog/config.js
@@ -318,15 +318,15 @@
         this.postId = $stateParams.id;
         // notification is the service used to display notifications on the top of the screen
         this.notification = notification;
-    };
+    }
     sendPostController.prototype.sendEmail = function() {
         if (this.email) {
             this.notification.log('Email successfully sent to ' + this.email, {addnCls: 'humane-flatty-success'});
         } else {
             this.notification.log('Email is undefined', {addnCls: 'humane-flatty-error'});
         }
-    }
-    sendPostController.inject = ['$stateParams', 'notification'];
+    };
+    sendPostController.$inject = ['$stateParams', 'notification'];
 
     var sendPostControllerTemplate =
         '<div class="row"><div class="col-lg-12">' +
