204b5da5188c4975cd3593071f038328bb99556c
Francois Zaninotto <fzaninotto@gmail.com>
2015-06-19 17:39:39 +0200

    Fix tests

--- src/javascripts/test/e2e/DashboardSpec.js
+++ src/javascripts/test/e2e/DashboardSpec.js
@@ -22,11 +22,12 @@ describe('Dashboard', function () {
 
     it('should display a panel for each entity with a list of recent items', function () {
         $$('.panel').then(function (panels) {
-            expect(panels.length).toBe(3);
+            expect(panels.length).toBe(4);
 
-            expect(panels[0].all(by.css('.panel-heading')).first().getText()).toBe('Recent posts');
-            expect(panels[1].all(by.css('.panel-heading')).first().getText()).toBe('Recent tags');
-            expect(panels[2].all(by.css('.panel-heading')).first().getText()).toBe('Last comments');
+            expect(panels[0].all(by.css('.panel-heading')).first().getText()).toBe('Last comments');
+            expect(panels[1].all(by.css('.panel-heading')).first().getText()).toBe('Recent posts');
+            expect(panels[2].all(by.css('.panel-heading')).first().getText()).toBe('Popular posts');
+            expect(panels[3].all(by.css('.panel-heading')).first().getText()).toBe('Tags publication status');
         });
     });
 });
