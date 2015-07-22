cc7496d2b85947e60bdb2ed21db7ef431f09b1ce
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2015-07-22 16:41:29 +0200

    Do not deduplicate single choice remote complete fields

--- src/javascripts/test/unit/Crud/repository/ReferenceRefresherSpec.js
+++ src/javascripts/test/unit/Crud/repository/ReferenceRefresherSpec.js
@@ -22,7 +22,8 @@ describe('ReferenceRefresher', function() {
             targetField: ()  => {
                 return { name: () => 'title' }
             },
-            getMappedValue: v => v
+            getMappedValue: v => v,
+            type: () => 'reference'
         };
     });
 
@@ -61,23 +62,43 @@ describe('ReferenceRefresher', function() {
             });
         });
 
-        it('should remove already selected values from result list (to fix some UI-Select duplicated options)', function(done) {
-            var readQueries = new ReadQueries();
-            sinon.stub(readQueries, 'getAllReferencedData', function() {
-                return mixins.buildPromise({
-                    post: [
-                        { id: 1, title: 'Discover some awesome stuff' },
-                        { id: 2, title: 'Another great post'}
-                    ]
+        describe('Choice deduplication (to fix some UI-Select duplicated options)', function() {
+            var refresher;
+            beforeEach(function() {
+                var readQueries = new ReadQueries();
+                sinon.stub(readQueries, 'getAllReferencedData', function() {
+                    return mixins.buildPromise({
+                        post: [
+                            { id: 1, title: 'Discover some awesome stuff' },
+                            { id: 2, title: 'Another great post'}
+                        ]
+                    });
+                });
+
+                refresher = new ReferenceRefresher(readQueries);
+            });
+
+            it('should remove already selected values from result list in case of multiple choices component', function(done) {
+                fakeField.type = () => 'reference_many';
+
+                refresher.refresh(fakeField, [1], 'foo').then(function(results) {
+                    expect(results).toEqual([
+                        { value: 2, label: 'Another great post' }
+                    ]);
+                    done();
                 });
             });
 
-            var refresher = new ReferenceRefresher(readQueries);
-            refresher.refresh(fakeField, [1], 'foo').then(function(results) {
-                expect(results).toEqual([
-                    { value: 2, label: 'Another great post' }
-                ]);
-                done();
+            it('should not de-duplicate simple choice in case of single choice component', function(done) {
+                fakeField.type = () => 'reference';
+
+                refresher.refresh(fakeField, [1], 'foo').then(function(results) {
+                    expect(results).toEqual([
+                        { value: 1, label: 'Discover some awesome stuff' },
+                        { value: 2, label: 'Another great post' }
+                    ]);
+                    done();
+                });
             });
         });
 
