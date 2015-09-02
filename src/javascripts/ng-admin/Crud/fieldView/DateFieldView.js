7b4a08e2680447eb8f45da0c1a1c5a4c6962a821
Manuel Francisco Naranjo <naranjo.manuel@gmail.com>
2015-09-02 20:10:00 -0300

    Fixing date widget in my forms

--- src/javascripts/ng-admin/Crud/fieldView/DateFieldView.js
+++ src/javascripts/ng-admin/Crud/fieldView/DateFieldView.js
@@ -2,5 +2,5 @@ module.exports = {
     getReadWidget:   () => '<ma-date-column field="::field" value="::entry.values[field.name()]"></ma-date-column>',
     getLinkWidget:   () => '<a ng-click="gotoDetail()">' + module.exports.getReadWidget() + '</a>',
     getFilterWidget: () => '<ma-date-field field="::field" value="values[field.name()]"></ma-date-field>',
-    getWriteWidget:  () =>'<div class="row"><ma-date-field field="::field" value="entry.values[field.name()]" class="col-sm-4"></ma-date-field></div>'
+    getWriteWidget:  () =>'<div class="row"><ma-date-field field="::field" value="entry.values[field.name()]" class="col-sm-11"></ma-date-field></div>'
 };
