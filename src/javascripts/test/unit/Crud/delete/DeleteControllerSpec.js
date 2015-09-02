c1e1e246ded5a417ae0fdb2a5a57adb0fa7bfb29
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2015-09-02 09:54:08 +0200

    Update tests (still in draft)

--- src/javascripts/test/unit/Crud/delete/DeleteControllerSpec.js
+++ src/javascripts/test/unit/Crud/delete/DeleteControllerSpec.js
@@ -5,55 +5,81 @@ describe('DeleteController', function () {
 
     var DeleteController = require('../../../../ng-admin/Crud/delete/DeleteController'),
         Entity = require('admin-config/lib/Entity/Entity'),
-        humane = require('humane-js'),
-        WriteQueries = require('admin-config/lib/Queries/WriteQueries');
+        humane = require('humane-js');
 
-    var deleteController;
-
-    var $scope, $window, $state, WriteQueries, notification, params, view, entry;
-    beforeEach(inject(function ($rootScope, _$window_, _WriteQueries_) {
+    var deleteController, $rootScope, $scope, $window, $state, writeQueries, notification, params, view, entry;
+    beforeEach(inject(function ($controller, $rootScope, _$window_) {
         $scope = $rootScope.$new();
         $window = _$window_;
-        // $state = _$state_;
-        WriteQueries = _WriteQueries_;
+        $state = {
+            go: sinon.spy(),
+            get: sinon.spy(),
+            params: {}
+        };
+        writeQueries = { deleteOne: () => {} };
         notification = humane;
         params = {
             id: 3,
-            entity: new Entity()
+            entity: new Entity('post')
         };
         view = {
             title: () => 'My view',
             description: () => 'Description',
             actions: () => [],
-            getEntity: () => new Entity()
+            getEntity: () => new Entity('post')
         };
         entry = {};
     }));
 
     describe('deleteOne', function() {
         describe('on success', function() {
-            fit('should delete given entity', function(done) {
-                var writeQueries = WriteQueries();
-                var deleteOneSpy = sinon.spy(writeQueries, 'deleteOne');
+            let writeQueries;
+            beforeEach(inject(function($q) {
+                writeQueries = { deleteOne: sinon.stub() };
+                writeQueries.deleteOne.returns($q.when());
+            }));
+
+            it('should delete given entity', function(done) {
                 var deleteController = new DeleteController($scope, $window, $state, writeQueries, notification, params, view, entry);
-                deleteController.deleteOne().then(function() {
-                    expect(deleteOneSpy).to.be.called();
+                deleteController.deleteOne(view, 3).then(function() {
+                    expect(writeQueries.deleteOne.calledOnce).toBeTruthy();
                     done();
                 }, done);
+
+                $scope.$digest();
             });
 
-            it('should redirect to entity list view', function(done) {
+            it('should redirect to entity list view if previous page is specific to deleted entity', function(done) {
+                let entity = new Entity('post');
+                let deletedId = 3;
+                let view = {
+                    title: () => 'Deleting a post',
+                    description: () => 'Remove a post',
+                    actions: () => [],
+                    getEntity: () => entity
+                };
+
+                let deleteController = new DeleteController($scope, $window, $state, writeQueries, notification, {
+                    id: deletedId,
+                    entity: 'post'
+                }, view, entry);
+
+                deleteController.deleteOne(view, 3).then(function() {
+                    expect($state.get.firstCall.args[0]).toBe('list');
+                    expect($state.go.firstCall.args[1]).toEqual({
+                        entity: 'post'
+                    });
+                    done();
+                }, done);
+
+                $scope.$digest();
             });
 
-            it('should display a success notification', function() {
-                deleteController.deleteOne().then(function() {
-
-                });
-                expect(deleteController.$scope.selection).toEqual([entries[0]]);
+            it('should redirect to previous page if not specific to deleted entity', function(done) {
             });
 
             afterEach(function() {
-                WriteQueries.deleteOne.restore();
+                sinon.restore();
             });
         });
 
