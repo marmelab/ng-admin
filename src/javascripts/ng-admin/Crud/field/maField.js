be18f4d070a13cf5c9606a5493a9cde28ec55030
Francois Zaninotto <fzaninotto@gmail.com>
2015-09-30 22:59:49 +0200

    Fix tests

--- src/javascripts/ng-admin/Crud/field/maField.js
+++ src/javascripts/ng-admin/Crud/field/maField.js
@@ -16,33 +16,6 @@ function maField(FieldViewConfiguration, $compile) {
             scope.entity = scope.entity();
             scope.form = scope.form();
             scope.datastore = scope.datastore();
-            var fieldTemplate;
-            if (scope.field.editable) {
-                fieldTemplate =
-`<div ng-class="getClassesForField(field, entry)">
-    ${scope.field.getTemplateValue(scope.entry) || FieldViewConfiguration[scope.type].getWriteWidget()}
-    <span ng-show="fieldHasValidation(field)" class="glyphicon form-control-feedback" ng-class="fieldIsValid(field) ? 'glyphicon-ok' : 'glyphicon-remove'"></span>
-</div>`;
-            } else {
-                fieldTemplate =
-`<div ng-class="field.getCssClasses(entry)||'col-sm-10'">
-    <p class="form-control-static">
-        <ma-column field="::field" entry="::entry" entity="::entity" datastore="::datastore"></ma-column>
-    </p>
-</div>`;
-            }
-
-            const template =
-`<div id="row-{{ field.name() }}" class="has-feedback" ng-class="getFieldValidationClass(field)">
-    <label for="{{ field.name() }}" class="col-sm-2 control-label">
-        {{ field.label() }}<span ng-if="field.validation().required">&nbsp;*</span>&nbsp;
-    </label>
-    ${fieldTemplate}
-</div>`;
-
-            element.append(template);
-            $compile(element.contents())(scope);
-
             scope.getClassesForField = function(field, entry) {
                 return 'ng-admin-field-' + field.name().replace('.', '_') + ' ' + 'ng-admin-type-' + field.type() + ' ' + (field.getCssClasses(entry) || 'col-sm-10 col-md-8 col-lg-7');
             };
@@ -74,6 +47,32 @@ function maField(FieldViewConfiguration, $compile) {
                 }
             };
 
+            var fieldTemplate;
+            if (scope.field.editable) {
+                fieldTemplate =
+`<div ng-class="getClassesForField(field, entry)">
+    ${scope.field.getTemplateValue(scope.entry) || FieldViewConfiguration[scope.type].getWriteWidget()}
+    <span ng-show="fieldHasValidation(field)" class="glyphicon form-control-feedback" ng-class="fieldIsValid(field) ? 'glyphicon-ok' : 'glyphicon-remove'"></span>
+</div>`;
+            } else {
+                fieldTemplate =
+`<div ng-class="field.getCssClasses(entry)||'col-sm-10'">
+    <p class="form-control-static">
+        <ma-column field="::field" entry="::entry" entity="::entity" datastore="::datastore"></ma-column>
+    </p>
+</div>`;
+            }
+
+            const template =
+`<div id="row-{{ field.name() }}" class="has-feedback" ng-class="getFieldValidationClass(field)">
+    <label for="{{ field.name() }}" class="col-sm-2 control-label">
+        {{ field.label() }}<span ng-if="field.validation().required">&nbsp;*</span>&nbsp;
+    </label>
+    ${fieldTemplate}
+</div>`;
+
+            element.append(template);
+            $compile(element.contents())(scope);
         }
     };
 }
