f3d88a5f207b71f5188d98d104ac10590bac6358
Francois Zaninotto <fzaninotto@gmail.com>
2015-10-20 21:04:00 +0200

    the non-required version is affected, too.

--- src/javascripts/ng-admin/Crud/fieldView/BooleanFieldView.js
+++ src/javascripts/ng-admin/Crud/fieldView/BooleanFieldView.js
@@ -3,7 +3,7 @@ module.exports = {
     getLinkWidget:   () => '<a ng-click="gotoDetail()">' + module.exports.getReadWidget() + '</a>',
     getFilterWidget: () => `<ma-choice-field field="::field" value="value" choices="[{value: 'true', label: 'true' }, { value: 'false', label: 'false' }]"></ma-choice-field>`,
     getWriteWidget:  () => `<div class="row">
-        <ma-choice-field class="col-sm-4 col-md-3" ng-if="!field.validation().required" field="::field" value="value"></ma-choice-field>
+        <ma-choice-field class="col-sm-4 col-md-3" ng-if="!field.validation().required" field="::field" value="$parent.value"></ma-choice-field>
         <ma-checkbox-field class="col-sm-4 col-md-3" ng-if="!!field.validation().required" field="::field" value="$parent.value"></ma-checkbox-field>
     </div>`
 }
