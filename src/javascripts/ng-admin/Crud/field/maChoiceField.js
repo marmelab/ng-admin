b7827fffd47799f0ed871a0082791490d52cf05c
Phil DeJarnett <phil@overzealous.com>
2015-11-12 11:23:37 -0500

    Fixed backwards scope.choices test.  Fixes #778.

--- src/javascripts/ng-admin/Crud/field/maChoiceField.js
+++ src/javascripts/ng-admin/Crud/field/maChoiceField.js
@@ -33,7 +33,7 @@ export default function maChoiceField($compile) {
                         refreshAttributes = 'refresh-delay="refreshDelay" refresh="refresh({ $search: $select.search })"';
                     }
 
-                    var choices = scope.choices() ? scope.choices : (field.choices ? field.choices() : []);
+                    var choices = scope.choices ? scope.choices() : (field.choices ? field.choices() : []);
                     var attributes = field.attributes();
                     scope.placeholder = (attributes && attributes.placeholder) || 'Filter values';
 
