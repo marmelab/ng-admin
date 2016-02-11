3543c6f339c28c2a7dc49e5269cc366034c1e4f4
Francois Zaninotto <fzaninotto@gmail.com>
2016-02-11 13:33:03 +0100

    Fix wrong closing tag in reference_many filter
    Refs #905

--- src/javascripts/ng-admin/Crud/fieldView/ReferenceManyFieldView.js
+++ src/javascripts/ng-admin/Crud/fieldView/ReferenceManyFieldView.js
@@ -1,6 +1,6 @@
 export default {
     getReadWidget:   () => '<ma-choices-column values="::entry.listValues[field.name()]"></ma-choices-column>',
     getLinkWidget:   () => '<ma-reference-many-link-column ids="::value" values="::entry.listValues[field.name()]" field="::field"></ma-reference-many-link-column>',
-    getFilterWidget: () => '<ma-reference-many-field field="::field" value="value" datastore="::datastore"></reference-many-field-field>',
+    getFilterWidget: () => '<ma-reference-many-field field="::field" value="value" datastore="::datastore"></ma-reference-many-field>',
     getWriteWidget:  () => '<ma-reference-many-field field="::field" value="value" datastore="::datastore"></ma-reference-many-field>'
 };
