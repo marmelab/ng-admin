f4a6b1dfac4df66126fd2e3fb50fc28323f56e91
Greg <phocea04500@hotmail.com>
2016-11-14 10:28:40 +0100

    Update HttpErrorService.js

--- src/javascripts/ng-admin/Main/component/provider/HttpErrorService.js
+++ src/javascripts/ng-admin/Main/component/provider/HttpErrorService.js
@@ -2,12 +2,14 @@ export default function httpErrorService( $state, $translate, notification) {
     
     return {
     	handle_error: function(event, toState, toParams, fromState, fromParams, error){
-    		if (error.status == 404) {
-    			this.handle_404_error(event,error)
+            switch (error.status) {
+            case 404:
+               this.handle_404_error();
+            case 403:
+               this.handle_403_error(error);
+            default:
+               this.handle_default_error(error);
             }
-    		else {
-    			this.handle_default_error(event,error)
-    		}
     	},
     	handle_404_error: function(event,error){
     		 $state.go('ma-404');
@@ -20,10 +22,8 @@ export default function httpErrorService( $state, $translate, notification) {
     	handle_default_error: function(event,error){
     		 $translate('STATE_CHANGE_ERROR', { message: error.message }).then(text => notification.log(text, { addnCls: 'humane-flatty-error' }));
              throw error;
-    		
     	}    	
     }
-
 }
 
 httpErrorService.$inject = ['$state', '$translate', 'notification'];
