3588e0e2c8d8a1942b0c9461ea902bd0a2a16ced
Greg <phocea04500@hotmail.com>
2016-12-09 18:30:03 +0100

    Update HttpErrorService.js

--- src/javascripts/ng-admin/Main/component/provider/HttpErrorService.js
+++ src/javascripts/ng-admin/Main/component/provider/HttpErrorService.js
@@ -1,33 +1,36 @@
-export default function httpErrorService($state, $translate, notification) {
-    
-    return {
-    	handleError: function(event, toState, toParams, fromState, fromParams, error){
-            switch (error.status) {
+const httpErrorService = ($state, $translate, notification) => ({
+    handleError: function(event, toState, toParams, fromState, fromParams, error) {
+        switch (error.status) {
             case 404:
-               this.handle404Error();
+               this.handle404Error(event, error);
                break;
             case 403:
                this.handle403Error(error);
                break;
             default:
                this.handleDefaultError(error);
-            break;
-            }
-    	},
-    	handle404Error: function(event, error){
-    		 $state.go('ma-404');
-             event.preventDefault();
-    	},
-    	handle403Error: function(error){
-    		 $translate('STATE_FORBIDDEN_ERROR', { message: error.data.message }).then(this.displayError);
-    		 throw error;
-    	},        
-    	handleDefaultError: function(error){
-    		 $translate('STATE_CHANGE_ERROR', { message: error.data.message }).then(this.displayError);
-             throw error;
-    	},
-        displayError:text => notification.log(text, { addnCls: 'humane-flatty-error' })
-    }
-}
+               break;
+        }
+    },
 
-httpErrorService.$inject =['$state', '$translate', 'notification'];
+    handle404Error: function(event) {
+        event.preventDefault();
+        $state.go('ma-404');
+    },
+
+    handle403Error: function(error) {
+        $translate('STATE_FORBIDDEN_ERROR', { message: error.data.message }).then(this.displayError);
+        throw error;
+    },
+
+    handleDefaultError: function(error) {
+        $translate('STATE_CHANGE_ERROR', { message: error.data.message }).then(this.displayError);
+         throw error;
+     },
+
+    displayError: text => notification.log(text, { addnCls: 'humane-flatty-error' }),
+});
+
+httpErrorService.$inject = ['$state', '$translate', 'notification'];
+
+export default { $get: httpErrorService };
