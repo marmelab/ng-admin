3f7609870fad847e5640f791f475f7f963bbdd7a
Kmaschta <kevin@marmelab.com>
2017-03-15 16:21:03 +0100

    Infinite scroll: avoid to load multiple time at the same time

--- src/javascripts/ng-admin/Crud/list/maDatagridInfinitePagination.js
+++ src/javascripts/ng-admin/Crud/list/maDatagridInfinitePagination.js
@@ -1,10 +1,14 @@
 import angular from 'angular';
 
-export default function maDatagridInfinitePagination($window, $document) {
+const isDownScrolling = wheelEvent => {
+    if (!wheelEvent) return true;
 
-    var windowElement = angular.element($window);
-    var offset = 100,
-        body = $document[0].body;
+    return wheelEvent.deltaY > 0;
+};
+
+export default function maDatagridInfinitePagination($window, $document) {
+    const offset = 100;
+    const body = $document[0].body;
 
     return {
         restrict: 'E',
@@ -13,28 +17,59 @@ export default function maDatagridInfinitePagination($window, $document) {
             totalItems: '@',
             nextPage: '&'
         },
-        link: function(scope) {
-            var perPage = parseInt(scope.perPage, 10) || 1,
-                totalItems = parseInt(scope.totalItems, 10),
-                nbPages = Math.ceil(totalItems / perPage) || 1,
-                page = 1,
-                loadedPages = [];
-            function handler() {
-                if (body.offsetHeight - $window.innerHeight - $window.scrollY < offset) {
-                    if (page >= nbPages) {
-                        return;
-                    }
-                    page++;
-                    if(page in loadedPages){
-                        return;
-                    }
-                    loadedPages.push(page);
-                    scope.nextPage()(page);
+        link(scope) {
+            const perPage = parseInt(scope.perPage, 10) || 1;
+            const totalItems = parseInt(scope.totalItems, 10);
+            const nbPages = Math.ceil(totalItems / perPage) || 1;
+            const loadedPages = [];
+            let page = 1;
+            let processing = false;
+            let interval;
+
+            const handler = (wheelEvent) => {
+                if (!isDownScrolling(wheelEvent) || processing || !!interval) {
+                    return;
                 }
-            }
-            windowElement.bind('scroll', handler);
-            scope.$on('$destroy', function destroy() {
-                windowElement.unbind('scroll', handler);
+
+                processing = true;
+
+                interval = setInterval(() => {
+                    if (body.offsetHeight - $window.innerHeight - $window.scrollY < offset) {
+                        if (page >= nbPages) {
+                            return;
+                        }
+
+                        page++;
+
+                        if (page in loadedPages) {
+                            return;
+                        }
+
+                        loadedPages.push(page);
+                        scope.nextPage()(page);
+                    } else {
+                        processing = false;
+
+                        if (interval) {
+                            clearInterval(interval);
+                            interval = null;
+                        }
+                    }
+                }, 100);
+            };
+
+            // Trigger the scroll at least one
+            // In this case, we'll avoid that a pagination of a few element (< 10)
+            // will never trigger the pagination
+            handler();
+
+            $window.addEventListener('wheel', handler);
+            scope.$on('$destroy', () => {
+                $window.removeEventListener('wheel', handler);
+
+                if (interval) {
+                    clearInterval(interval);
+                }
             });
         }
     };
