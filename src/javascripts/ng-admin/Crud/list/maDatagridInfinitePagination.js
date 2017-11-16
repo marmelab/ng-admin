7ea821985086c2d2121d5b90aa979586ce9b04f8
Florian FERBACH <florian@marmelab.com>
2017-11-16 15:31:33 +0100

    Debounce scroll events

--- src/javascripts/ng-admin/Crud/list/maDatagridInfinitePagination.js
+++ src/javascripts/ng-admin/Crud/list/maDatagridInfinitePagination.js
@@ -1,4 +1,5 @@
 import angular from 'angular';
+import debounce from 'lodash.debounce';
 
 const isScrollingDown = wheelEvent => {
     if (!wheelEvent) return true;
@@ -26,7 +27,7 @@ export default function maDatagridInfinitePagination($window, $document) {
             let page = 1;
             let interval;
 
-            const handler = (wheelEvent) => {
+            const handler = debounce((wheelEvent) => {
                 if (!isScrollingDown(wheelEvent) || scope.processing || !!interval) {
                     return;
                 }
@@ -56,7 +57,7 @@ export default function maDatagridInfinitePagination($window, $document) {
                         }
                     }
                 }, 100);
-            };
+            }, 500, { maxWait: 1000 });
 
             // Trigger the scroll at least once
             // This way, it loads at least one screen of data to enable further scrolling
